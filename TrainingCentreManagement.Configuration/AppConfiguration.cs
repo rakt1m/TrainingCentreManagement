78e62705f34bca3a837b2ef2e00cbc630fe28af6
shraktim <shahadathossainraktim@gmail.com>
2018-12-28 21:01:15 +0600

    layerDesign

---
+++ TrainingCentreManagement.Configuration/AppConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TrainingCentreManagement.DatabaseContext.DatabaseContext;
+
+
+namespace TrainingCentreManagement.Configuration
+{
+   public static class AppConfiguration
+    {
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(IConfiguration.GetConnectionString("ApplicationDbContext") as DbConnection, b => b.MigrationsAssembly("TrainingCentreManagement.DatabaseContext")));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+    }
+}
---
+++ TrainingCentreManagement.DatabaseContext/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainingCentreManagement.DatabaseContext.DatabaseContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+    }
+}
