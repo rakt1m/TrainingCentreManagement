4444923e4bee573f8a1f4cd27a48ea52f16557c2
shraktim <shahadathossainraktim@gmail.com>
2018-12-28 20:04:22 +0600

    project created

---
+++ TrainingCentreManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrainingCentreManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
